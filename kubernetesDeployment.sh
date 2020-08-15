eb4d21db05130d64647beee6b7ff5b7da88b879a
Fernando Juarez <luis.juarez@autoweb.com>
2020-08-15 13:08:40 -0600

    testing with the complete path5

--- kubernetesDeployment.sh
+++ kubernetesDeployment.sh
@@ -1,11 +1,11 @@
-/home/ubuntu/kubectlkubectl apply -f $1
+/home/ubuntu/kubectl apply -f $1
 
-/home/ubuntu/kubectlkubectl set image deployments/$2 $3=$4
+/home/ubuntu/kubectl set image deployments/$2 $3=$4
 
-/home/ubuntu/kubectlkubectl expose deployment/$2 \
+/home/ubuntu/kubectl expose deployment/$2 \
 --name=$5 \
 --type="LoadBalancer" \
 --port=$6 \
 --target-port=$7
 
-/home/ubuntu/kubectlkubectl describe services $5
+/home/ubuntu/kubectl describe services $5
