724bcd99e4b592bb6aaf8773e3365007558d66f0
Fernando Juarez <luis.juarez@autoweb.com>
2020-08-16 10:50:31 -0600

    adding the service to the deployment file

--- kubernetesDeployment.sh
+++ kubernetesDeployment.sh
@@ -2,10 +2,4 @@
 
 /home/ubuntu/kubectl set image deployments/$2 $3=$4
 
-/home/ubuntu/kubectl expose deployment/$2 \
---name=$5 \
---type="LoadBalancer" \
---port=$6 \
---target-port=$7
-
 /home/ubuntu/kubectl describe services $5
