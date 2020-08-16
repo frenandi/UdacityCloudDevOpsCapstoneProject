14ec60ac809215ef8fb6a495cb55d6146de26340
Fernando Juarez <luis.juarez@autoweb.com>
2020-08-16 14:03:17 -0600

    remove index.html

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_www():
-    return render_template("index.html")
+    return "Hello Udacity!!!"
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=80)
